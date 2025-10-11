6962fe92541acb45bf5139de61eda784f331f3b9
LeoTatarin <tatarinleo@gmail.com>
2025-10-11 10:24:42 -0300

    removida parte console.log

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,6 @@ const contaAsHoras = setInterval(function() {
     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
 
-    console.log(diasAteOEvento);
-    console.log(horasAteOEvento);
-    console.log(minutosAteOEvento);
-    console.log(segundosAteOEvento);
 
     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
 
